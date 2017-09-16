7910e4b873e3247ed3f99a0dcb77c1d03792ffe1
yunus <Tundu@email.com>
2017-09-16 11:45:26 +0800

    链表

--- src/com/yunus/chain/Link.java
+++ src/com/yunus/chain/Link.java
@@ -4,13 +4,13 @@ package com.yunus.chain;
  * Created by yunus on 2017/9/14.
  * 链表
  */
-public class Link {
+public class Link<E> {
 
     // 第一个数据
     private Node first;
 
     // 插入
-    public void insert(long value) {
+    public void insert(E value) {
         Node lin = new Node(value);
         if (first == null) {
             first = lin;
@@ -23,9 +23,37 @@ public class Link {
     // 查询
     public void displayAll() {
         Node current = first;
-        while(current!=null){
+        while (current != null) {
             System.out.println(current.getData());
             current = current.getNext();
         }
     }
+
+    static class Node<E> {
+        // 数据域
+        private E data;
+        // 指针域
+        private Node next;
+
+        public Node(E data) {
+            this.data = data;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
 }
--- src/com/yunus/chain/Test.java
+++ src/com/yunus/chain/Test.java
@@ -7,16 +7,6 @@ public class Test {
     public static void main(String[] args) {
         test2();
     }
-    public static void test1(){
-        Node link1 = new Node(1);
-        Node link2 = new Node(2);
-        Node link3 = new Node(3);
-        Node link4 = new Node(4);
-        link1.setNext(link2);
-        link2.setNext(link3);
-        link3.setNext(link4);
-
-    }
 
     public static void test2(){
         Link link = new Link();
