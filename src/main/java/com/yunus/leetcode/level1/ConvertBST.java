1c7419e738cb3ff820a33436aa25aceb7871d10e
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-07-16 10:32:26 +0800

    二叉搜索树转换为累加树

--- src/main/java/com/yunus/leetcode/level1/ConvertBST.java
+++ src/main/java/com/yunus/leetcode/level1/ConvertBST.java
@@ -2,6 +2,8 @@ package com.yunus.leetcode.level1;
 
 import com.yunus.leetcode.TreeNode;
 
+import java.util.Stack;
+
 /**
  * @author gaoyunfeng
  * @Description: 二叉搜索树转换为累加树
@@ -16,6 +18,27 @@ public class ConvertBST {
             sum += root.val;
             root.val = sum;
             convertBST(root.left);
+        }
+        return root;
+    }
+
+    public TreeNode convertBST2(TreeNode root) {
+        int sum = 0;
+        TreeNode node = root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        while (!stack.isEmpty() || node != null) {
+            // 压入所有的右节点
+            while (node != null) {
+                stack.add(node);
+                node = node.right;
+            }
+
+            node = stack.pop();
+            sum += node.val;
+            node.val = sum;
+            // 压入所有的左节点
+            node = node.left;
         }
         return root;
     }
