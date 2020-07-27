245f613f49aaf0c25725706c21670e4a6f82a41b
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-07-27 10:30:46 +0800

    最小栈

---
+++ src/main/java/com/yunus/leetcode/level1/MinStack.java
@@ -0,0 +1,63 @@
+package com.yunus.leetcode.level1;
+
+import java.util.Stack;
+
+/**
+ * @author gaoyunfeng
+ * @Description: 最小栈
+ * @date 2020/7/2710:20
+ */
+public class MinStack {
+
+    private Stack<Integer> data;
+    private Stack<Integer> helper;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+        data = new Stack<>();
+        helper = new Stack<>();
+    }
+
+    public void push(int x) {
+        // 数据栈和辅助栈一定会增加元素
+        data.add(x);
+        if (helper.isEmpty() || helper.peek() >= x) {
+            helper.add(x);
+        } else {
+            helper.add(helper.peek());
+        }
+    }
+
+    public void pop() {
+        // 两个栈都得 pop
+        if (!data.isEmpty()) {
+            helper.pop();
+            data.pop();
+        }
+    }
+
+    public int top() {
+        if(!data.isEmpty()){
+            return data.peek();
+        }
+        throw new RuntimeException("栈中元素为空，此操作非法");
+    }
+
+    public int getMin() {
+        if(!helper.isEmpty()){
+            return helper.peek();
+        }
+        throw new RuntimeException("栈中元素为空，此操作非法");
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
