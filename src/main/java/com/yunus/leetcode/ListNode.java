4636a544a761cd25c2a3cff618618fe1d6175830
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-08-07 12:07:40 +0800

    链表排序

---
+++ src/main/java/com/yunus/leetcode/ListNode.java
@@ -0,0 +1,17 @@
+package com.yunus.leetcode;
+
+import lombok.Getter;
+
+/**
+ * @author gaoyunfeng
+ * @Description: 链表
+ * @date 2020/8/617:00
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/yunus/leetcode/level2/SortList.java
@@ -0,0 +1,56 @@
+package com.yunus.leetcode.level2;
+
+import com.yunus.leetcode.ListNode;
+
+/**
+ * @author gaoyunfeng
+ * @Description: 排序列表
+ * 在O(nlogn) 时间复杂度和常数级空间复杂度下，对链表进行排序。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例 2:
+ * <p>
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ * @date 2020/8/616:58
+ */
+public class SortList {
+
+    /**
+     * 归并排序（递归法）
+     *
+     * @param head 链表头节点
+     * @return
+     */
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+}
