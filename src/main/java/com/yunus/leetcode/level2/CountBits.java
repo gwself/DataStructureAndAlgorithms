d6131b229e86e9132f6b2a64a3778ae459b1037b
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-08-12 14:08:44 +0800

    比特位计数

---
+++ src/main/java/com/yunus/leetcode/level2/CountBits.java
@@ -0,0 +1,45 @@
+package com.yunus.leetcode.level2;
+
+/**
+ * @author gaoyunfeng
+ * @Description: 比特位计数
+ * 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 2
+ * 输出: [0,1,1]
+ * 示例 2:
+ * <p>
+ * 输入: 5
+ * 输出: [0,1,1,2,1,2]
+ * 进阶:
+ * <p>
+ * 给出时间复杂度为O(n*sizeof(integer))的解答非常容易。但你可以在线性时间O(n)内用一趟扫描做到吗？
+ * 要求算法的空间复杂度为O(n)。
+ * 你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的 __builtin_popcount）来执行此操作。
+ * @date 2020/8/1213:55
+ */
+public class CountBits {
+
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 0; i <= num; ++i) {
+            ans[i] = popcount(i);
+        }
+        return ans;
+    }
+
+    private int popcount(int x) {
+        int count;
+        for (count = 0; x != 0; ++count)
+            //zeroing out the least significant nonzero bit
+        {
+            x &= x - 1;
+            x = x&(x-1);
+        }
+        return count;
+    }
+
+
+}
