9bdbc503d67f9d92ea74e902a37f16f1fb0f739e
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-09-14 15:58:52 +0800

    旋转数组

--- src/main/java/com/yunus/leetcode/level1/RotateArray.java
+++ src/main/java/com/yunus/leetcode/level1/RotateArray.java
@@ -14,7 +14,27 @@ public class RotateArray {
     }
 
     public static void rotate(int[] nums, int k) {
+        int[] a = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            a[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = a[i];
+        }
+    }
 
+    public static void rotate2(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            offsetOne(nums);
+        }
+    }
+
+    public static void offsetOne(int[] arr) {
+        int last = arr[arr.length - 1];
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
     }
 
 }
