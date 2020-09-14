8940712ea68441c9ac220025299f44a8da9669c7
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-09-14 15:48:32 +0800

    旋转数组

---
+++ src/main/java/com/yunus/leetcode/level1/RotateArray.java
@@ -0,0 +1,20 @@
+package com.yunus.leetcode.level1;
+
+/**
+ * @author gaoyunfeng
+ * @Description: 旋转数组
+ * @date 2020/9/14 15:44
+ */
+public class RotateArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        rotate(arr, k);
+    }
+
+    public static void rotate(int[] nums, int k) {
+
+    }
+
+}
