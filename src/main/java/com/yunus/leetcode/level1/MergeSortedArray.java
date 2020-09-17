891a6f6b63d1e234ba4df3248dbeaf0d1a16cd76
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-09-17 14:51:39 +0800

    双指针  从后往前 合并两个有序数组

---
+++ src/main/java/com/yunus/leetcode/level1/MergeSortedArray.java
@@ -0,0 +1,43 @@
+package com.yunus.leetcode.level1;
+
+/**
+ * @author gaoyunfeng
+ * @Description: 归并两个有序数组
+ * @date 2020/9/17 13:50
+ */
+public class MergeSortedArray {
+
+    /**
+     * 从后往前
+     * 例示 输入:
+     * nums1 = [1,2,3,0,0,0], m = 3
+     * nums2 = [2,5,6],       n = 3
+     * <p>
+     * 输出: [1,2,2,3,5,6]
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums1 数组1
+     * @param m     有效长度
+     * @param nums2 数组2
+     * @param n     数组2 有效长度
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // two get pointers for nums1 and nums2
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // set pointer for nums1
+        int p = m + n - 1;
+
+        // while there are still elements to compare
+        while ((p1 >= 0) && (p2 >= 0))
+            // compare two elements from nums1 and nums2
+            // and add the largest one in nums1
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+        // add missing elements from nums2
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+    }
+}
