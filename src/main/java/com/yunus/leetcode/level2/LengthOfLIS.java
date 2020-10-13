55e1fe66b30c46d97db5e07694d229c8802316f8
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-10-13 17:16:11 +0800

    300. 最长上升子序列

---
+++ src/main/java/com/yunus/leetcode/level2/LengthOfLIS.java
@@ -0,0 +1,28 @@
+package com.yunus.leetcode.level2;
+
+/**
+ * @author gaoyunfeng
+ * @Description: 300. 最长上升子序列
+ * @date 2020/10/13 11:16
+ */
+public class LengthOfLIS {
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+}
