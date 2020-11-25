f8650f8721210e3c94f882b8d932ca789efbe1a3
gaoyunfeng <gaoyunfeng200420@credithc.com>
2020-11-25 15:38:22 +0800

    test

---
+++ src/main/java/com/yunus/foo/Test.java
@@ -0,0 +1,12 @@
+package com.yunus.foo;
+
+/**
+ * @author gaoyunfeng
+ * @Description:
+ * @date 2020/11/25 15:34
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
