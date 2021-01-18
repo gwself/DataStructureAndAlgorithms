a69d18deb5a57a5cb299638ff1b746373413f5e5
gaoyunfeng <gaoyunfeng200420@credithc.com>
2021-01-18 16:51:19 +0800

    14. 最长公共前缀

---
+++ src/main/java/com/yunus/leetcode/level1/LongestCommonPrefix.java
@@ -0,0 +1,54 @@
+package com.yunus.leetcode.level1;
+
+/**
+ * @author gaoyunfeng
+ * @Description: 14. 最长公共前缀
+ * @date 2021/1/18 16:46
+ */
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String ans = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            for (; j < ans.length() && j < strs[i].length(); j++) {
+                if (ans.charAt(j) != strs[i].charAt(j)) {
+                    break;
+                }
+            }
+            ans = ans.substring(0, j);
+            if (ans.equals("")) {
+                return ans;
+            }
+        }
+        return ans;
+    }
+
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+
+}
