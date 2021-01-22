df333fb4367364dc807f9fa102406add831e5303
gaoyunfeng <gaoyunfeng200420@credithc.com>
2021-01-22 10:55:59 +0800

    101. 对称二叉树

--- src/main/java/com/yunus/leetcode/level1/IsSymmetric.java
+++ src/main/java/com/yunus/leetcode/level1/IsSymmetric.java
@@ -3,7 +3,6 @@ package com.yunus.leetcode.level1;
 import com.yunus.leetcode.TreeNode;
 
 import java.util.LinkedList;
-import java.util.Queue;
 
 /**
  * @author gaoyunfeng
@@ -15,55 +14,64 @@ public class IsSymmetric {
 
 
     public boolean isSymmetric(TreeNode root) {
-        return check(root, root);
-    }
-
-    /**
-     * 递归
-     *
-     * @param p
-     * @param q
-     * @return
-     */
-    public boolean check(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
+        if (root == null) {
             return true;
         }
-        if (p == null || q == null) {
+        //调用递归函数，比较左节点，右节点
+        return dfs(root.left, root.right);
+    }
+
+    boolean dfs(TreeNode left, TreeNode right) {
+        //递归的终止条件是两个节点都为空
+        //或者两个节点中有一个为空
+        //或者两个节点的值不相等
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
             return false;
         }
-        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+        if (left.val != right.val) {
+            return false;
+        }
+        //再递归的比较 左节点的左孩子 和 右节点的右孩子
+        //以及比较  左节点的右孩子 和 右节点的左孩子
+        return dfs(left.left, right.right) && dfs(left.right, right.left);
     }
 
-    /**
-     * 迭代
-     *
-     * @param u
-     * @param v
-     * @return
-     */
-    public boolean check2(TreeNode u, TreeNode v) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(u);
-        q.offer(v);
-        while (!q.isEmpty()) {
-            u = q.poll();
-            v = q.poll();
-            if (u == null && v == null) {
+
+    public boolean isSymmetric2(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        //用队列保存节点
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        //将根节点的左右孩子放到队列中
+        queue.add(root.left);
+        queue.add(root.right);
+        while (queue.size() > 0) {
+            //从队列中取出两个节点，再比较这两个节点
+            TreeNode left = queue.removeFirst();
+            TreeNode right = queue.removeFirst();
+            //如果两个节点都为空就继续循环，两者有一个为空就返回false
+            if (left == null && right == null) {
                 continue;
             }
-            if ((u == null || v == null) || (u.val != v.val)) {
+            if (left == null || right == null) {
                 return false;
             }
-
-            q.offer(u.left);
-            q.offer(v.right);
-
-            q.offer(u.right);
-            q.offer(v.left);
+            if (left.val != right.val) {
+                return false;
+            }
+            //将左节点的左孩子， 右节点的右孩子放入队列
+            queue.add(left.left);
+            queue.add(right.right);
+            //将左节点的右孩子，右节点的左孩子放入队列
+            queue.add(left.right);
+            queue.add(right.left);
         }
+
         return true;
     }
-
 
 }
