4260db6dce9bd90ba523fe96a948b1c6ae04363b
gaoyunfeng <gaoyunfeng200420@credithc.com>
2021-01-25 11:23:14 +0800

    22. 括号生成

--- src/main/java/com/yunus/foo/Test.java
+++ src/main/java/com/yunus/foo/Test.java
@@ -7,6 +7,17 @@ package com.yunus.foo;
  */
 public class Test {
     public static void main(String[] args) {
+        Test test = new Test();
+        test.test("hello world");
+    }
 
+    public void test(String str) {
+        if (str.length() == 0) {
+            return;
+        } else {
+            str = str.substring(0, str.length() - 1);
+            test(str);
+            System.out.println(str);
+        }
     }
 }
--- src/main/java/com/yunus/leetcode/level1/GenerateParenthesis.java
+++ src/main/java/com/yunus/leetcode/level1/GenerateParenthesis.java
@@ -11,51 +11,7 @@ import java.util.List;
  */
 public class GenerateParenthesis {
 
-    private final char left = '(';
-    private final char right = ')';
-
-    /**
-     * 暴力法
-     *
-     * @param n 括号个数
-     * @return
-     */
     public List<String> generateParenthesis(int n) {
-        List<String> combinations = new ArrayList<String>();
-        generateAll(new char[2 * n], 0, combinations);
-        return combinations;
-    }
-
-    public void generateAll(char[] current, int pos, List<String> result) {
-        if (pos == current.length) {
-            if (valid(current)) {
-                result.add(new String(current));
-            }
-        } else {
-            current[pos] = left;
-            generateAll(current, pos + 1, result);
-            current[pos] = right;
-            generateAll(current, pos + 1, result);
-        }
-    }
-
-    public boolean valid(char[] current) {
-        int balance = 0;
-        for (char c : current) {
-            if (c == '(') {
-                ++balance;
-            } else {
-                --balance;
-            }
-            if (balance < 0) {
-                return false;
-            }
-        }
-        return balance == 0;
-    }
-
-
-    public List<String> generateParenthesis2(int n) {
         List<String> res = new ArrayList<>();
         // 特判
         if (n == 0) {
@@ -92,6 +48,74 @@ public class GenerateParenthesis {
 
         if (right > 0) {
             dfs(curStr + ")", left, right - 1, res);
+        }
+    }
+
+    /**
+     * 暴力法
+     *
+     * @param n
+     * @return
+     */
+    public List<String> generateParenthesis2(int n) {
+        List<String> combinations = new ArrayList<String>();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+    }
+
+    public void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current)) {
+                result.add(new String(current));
+            }
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos + 1, result);
+            current[pos] = ')';
+            generateAll(current, pos + 1, result);
+        }
+    }
+
+    public boolean valid(char[] current) {
+        int balance = 0;
+        for (char c : current) {
+            if (c == '(') {
+                ++balance;
+            } else {
+                --balance;
+            }
+            if (balance < 0) {
+                return false;
+            }
+        }
+        return balance == 0;
+    }
+
+    public static void main(String[] args) {
+        GenerateParenthesis generateParenthesis = new GenerateParenthesis();
+        generateParenthesis.generateParenthesis2(3);
+    }
+
+    public List<String> generateParenthesis3(int n) {
+        List<String> ans = new ArrayList<String>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
         }
     }
 
