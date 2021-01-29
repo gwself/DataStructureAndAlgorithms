5c3c3948dd7cbcdc17887720ee7104108def6427
gaoyunfeng <gaoyunfeng200420@credithc.com>
2021-01-29 14:12:02 +0800

    feat(ReverseString): 344. 反转字符串

---
+++ src/main/java/com/yunus/leetcode/level1/ReverseString.java
@@ -0,0 +1,21 @@
+package com.yunus.leetcode.level1;
+
+/**
+ * @author gaoyunfeng
+ * @Description: 344. 反转字符串
+ * @date 2021/1/29 14:05
+ */
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length;
+        while (left < right) {
+            // 交换
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right++;
+        }
+    }
+}
