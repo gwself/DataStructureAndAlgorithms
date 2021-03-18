1610a10284b8a1db07cf9c25c042d41ca9dfa9b3
gaoyunfeng <gaoyunfeng200420@credithc.com>
2021-03-18 10:16:36 +0800

    feat(Heap): 堆

---
+++ src/main/java/com/yunus/heap/Heap.java
@@ -0,0 +1,87 @@
+package com.yunus.heap;
+
+/**
+ * @author gaoyunfeng
+ * @Description:
+ * @date 2021/3/18 9:58
+ */
+public class Heap {
+
+    /**
+     * 数组，从下标1开始存储数据
+     */
+    private int[] a;
+    /**
+     * 堆可以存储的最大数据个数
+     */
+    private int n;
+    /**
+     * 堆中已经存储的数据个数
+     */
+    private int count;
+
+    public Heap(int capacity) {
+        a = new int[capacity + 1];
+        n = capacity;
+        count = 0;
+    }
+
+    public void insert(int data) {
+        // 堆满了
+        if (count >= n) {
+            return;
+        }
+        ++count;
+        a[count] = data;
+        int i = count;
+        // 自下往上堆化
+        while (i / 2 > 0 && a[i] > a[i / 2]) {
+            // swap()函数作用：交换下标为i和i/2的两个元素
+            swap(a, i, i / 2);
+            i = i / 2;
+        }
+    }
+
+
+    public void removeMax() {
+        // 堆中没有数据
+        if (count == 0) {
+            return;
+        }
+        a[1] = a[count];
+        --count;
+        heapify(a, count, 1);
+    }
+
+    private void heapify(int[] a, int n, int i) {
+        // 自上往下堆化
+        while (true) {
+            int maxPos = i;
+            if (i * 2 <= n && a[i] < a[i * 2]) {
+                maxPos = i * 2;
+            }
+            if (i * 2 + 1 <= n && a[maxPos] < a[i * 2 + 1]) {
+                maxPos = i * 2 + 1;
+            }
+            if (maxPos == i) {
+                break;
+            }
+            swap(a, i, maxPos);
+            i = maxPos;
+        }
+    }
+
+
+    private void buildHeap(int[] a, int n) {
+        for (int i = n / 2; i >= 1; --i) {
+            heapify(a, n, i);
+        }
+    }
+
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
